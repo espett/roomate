531f2715f7955c89f1506ee3aadcff76c6dd1424
Eric Spett <eric.spett@gmail.com>
2013-10-09 19:19:32 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Roomate::Application.config.secret_token = '14aee239edda78fcdfa846fc4f9f10ff0f45c19c9137d1abf377b17167e78e7e3d43400037282d847141b0632bd5c37953ca78ce316f0eafd501d4be0db04afb'
